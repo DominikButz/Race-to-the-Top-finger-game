84351df699494d1c5ef12d865e7ed0b66b57d430
Dominik <dominikbutz@gmail.com>
2013-11-18 13:28:53 +0800

    Initial Commit

---
+++ Race to the Top/RTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RTAppDelegate.h
+//  Race to the Top
+//
+//  Created by Dominik Butz on 18.11.13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
